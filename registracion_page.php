4887d59ab8eecd4abdafb3f238543c0d366201f3
tumba <tumbaszbarbi@gamil.com>
2025-07-05 13:14:46 +0200

    regisztrációs oldal 1.0

---
+++ registracion_page.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Regisztráció</title>
+</head>
+<body>
+<form id="" action="" method="">
+<!--    Last Name-->
+    <label for="">Vezetéknév</label>
+    <input type="text" id="" name="">
+
+<!--    First Name-->
+    <label for="">Keresztnév</label>
+    <input type="text" id="" name="">
+
+<!--    Email-->
+    <label for="">Email</label>
+    <input type="email" id="" name="">
+
+<!--    Password-->
+    <label for="">Jelszó</label>
+    <input type="password" id="" name="">
+
+<!--    Password Confirm-->
+    <label for="">Jelszó megerősítés</label>
+    <input type="password" id="" name="">
+
+<!--    Phone number-->
+    <label for="">Telefonszám</label>
+    <input type="text" id="" name="">
+
+<!--    Birthday-->
+    <label for="">Születési dátum</label>
+    <input type="date" id="" name="">
+
+<!--    Button-->
+    <button type="submit">Regisztráció</button>
+    <button type="reset">Törlés</button>
+</form>
+</body>
+</html><?php
