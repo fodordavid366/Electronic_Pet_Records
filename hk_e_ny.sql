e65a0aaf2afc7b41f4d38be0b682575fe1e9a53f
fodor <fodordavid366@gmail.com>
2025-09-03 07:33:07 +0200

    database

--- hk_e_ny.sql
+++ hk_e_ny.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Aug 31. 00:42
+-- Létrehozás ideje: 2025. Sze 03. 07:31
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -37,6 +37,13 @@ CREATE TABLE `admin` (
   `phone_number` varchar(13) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- A tábla adatainak kiíratása `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `email`, `password`, `first_name`, `last_name`, `birth_date`, `phone_number`) VALUES
+(1, 'testadmin@gmail.com', '$2y$10$060JDWhTGfvs.HczZTBMI.lFS.QPILhs/MVFpDuqpyAR2KhNG1Ydq', 'David', 'Fodor', '2003-08-07', '1234567891');
+
 -- --------------------------------------------------------
 
 --
@@ -51,8 +58,26 @@ CREATE TABLE `appointment` (
   `starts_at` datetime NOT NULL,
   `ends_at` datetime NOT NULL,
   `treatment_id` int(11) NOT NULL,
-  `status` enum('booked','cancelled','completed') NOT NULL DEFAULT 'booked'
+  `status` enum('booked','canceled','completed') NOT NULL DEFAULT 'booked'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `appointment`
+--
+
+INSERT INTO `appointment` (`appointment_id`, `pet_id`, `vet_id`, `description`, `starts_at`, `ends_at`, `treatment_id`, `status`) VALUES
+(9, 18, 1, '', '2025-09-01 13:30:00', '2025-09-01 13:45:00', 1, 'booked'),
+(10, 18, 1, '', '2025-09-01 15:00:00', '2025-09-01 16:00:00', 2, 'booked'),
+(11, 18, 1, '', '2025-08-31 21:30:00', '2025-08-31 21:45:00', 1, 'booked'),
+(12, 19, 1, '', '2003-09-01 10:00:00', '2003-09-01 10:15:00', 1, 'booked'),
+(13, 19, 1, '', '2025-09-01 16:00:00', '2025-09-01 16:15:00', 1, 'booked'),
+(14, 18, 1, '', '2003-09-01 09:00:00', '2003-09-01 09:15:00', 1, 'booked'),
+(15, 10, 2, 'asdasd', '2025-09-01 09:00:00', '2025-09-01 09:15:00', 1, 'completed'),
+(16, 18, 1, '', '2025-09-01 10:00:00', '2025-09-01 10:15:00', 1, 'booked'),
+(18, 10, 2, '', '2025-09-09 09:30:00', '2025-09-09 10:30:00', 2, 'booked'),
+(19, 18, 1, '', '2025-09-08 10:00:00', '2025-09-08 11:00:00', 2, 'booked'),
+(20, 10, 2, '', '2025-09-08 09:00:00', '2025-09-08 10:00:00', 2, 'booked'),
+(21, 18, 1, '', '2025-09-08 15:00:00', '2025-09-08 15:15:00', 1, 'booked');
 
 -- --------------------------------------------------------
 
@@ -70,6 +95,14 @@ CREATE TABLE `log` (
   `proxy` tinyint(1) NOT NULL,
   `isp` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- A tábla adatainak kiíratása `log`
+--
+
+INSERT INTO `log` (`id_log`, `user_agent`, `ip_address`, `country`, `date_time`, `device_type`, `proxy`, `isp`) VALUES
+(10, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36', '::1', '', '2025-09-03 06:40:42', 'computer', 0, ''),
+(11, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36', '8.8.8.8', 'United States', '2025-09-03 06:42:18', 'computer', 0, 'Google LLC');
 
 -- --------------------------------------------------------
 
@@ -91,21 +124,19 @@ CREATE TABLE `owner` (
   `forgotten_password_token` varchar(255) NOT NULL,
   `forgotten_password_token_expires` datetime NOT NULL,
   `is_banned` tinyint(1) NOT NULL DEFAULT 0,
-  `email_notification` tinyint(1) NOT NULL DEFAULT 0
+  `email_notify` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- A tábla adatainak kiíratása `owner`
 --
 
-INSERT INTO `owner` (`owner_id`, `email`, `password`, `first_name`, `last_name`, `phone_number`, `birth_date`, `registration_token`, `registration_token_expires`, `verified`, `forgotten_password_token`, `forgotten_password_token_expires`, `is_banned`, `email_notification`) VALUES
+INSERT INTO `owner` (`owner_id`, `email`, `password`, `first_name`, `last_name`, `phone_number`, `birth_date`, `registration_token`, `registration_token_expires`, `verified`, `forgotten_password_token`, `forgotten_password_token_expires`, `is_banned`, `email_notify`) VALUES
 (1, 'teszt@valami.hu', '$2y$10$Kp4aodpsl9.oiE.z3q5uE.lWSwnH3LFiSJ57SwOZgOWP0IYggcJ3a', '', '', '', '0000-00-00', 'd28bd6487895f2e0a1cf65be9e3876ae7d39c7fd300e9a2f2ca60ee3b932e658', '0000-00-00 00:00:00', 0, '', '0000-00-00 00:00:00', 0, 0),
 (2, 'pelda@pelda.hu', '$2y$10$jcekjSgzvZD6vMkyfALv6ukzMHF7oucd4FrM9vbm6QtHxU0Yqsoqy', 'Kovács', 'János', '+36301234567', '1990-05-15', '3de9ad73846f8f14e041ac988cc2f41fb594238142e0478f4903dbd1394e133d', '2025-07-07 11:35:21', 0, '', '0000-00-00 00:00:00', 0, 0),
-(3, 'pelda@pelda1.hu', '$2y$10$D0HaqRRgD9FgmvvXLjCI3uasQONuNxHHwZhNOV9SIGnbRXiJbJoMS', 'Kovács', 'János', '+36301234567', '1990-05-15', '46c3a7340738e37c7ee66dfe5b0da24af4bbff3bc0e885d2bf9a9c46e4d231c2', '2025-07-07 11:36:15', 0, '', '0000-00-00 00:00:00', 0, 0),
 (4, 'pelda@pelda12.hu', '$2y$10$8RQW7emH9kmc3iIlRD8tveacix3nB/DasmCIAQjMPtQnFOI0xyuSe', 'Kovács124123123', 'János', '+36301234567', '1990-05-15', '', '0000-00-00 00:00:00', 1, '', '0000-00-00 00:00:00', 0, 0),
 (5, 'pelda@peld123a.hu', '$2y$10$BEs6T6Q8INmmETGq8yOlnejMM5xMU.NXyDFcmZ0j.urDlxZjRNOPm', 'Géza', 'János', '+3630124567', '1990-05-15', '', '0000-00-00 00:00:00', 1, '', '0000-00-00 00:00:00', 0, 0),
-(6, 'ujowner@pelda.hu', '$2y$10$RJklv4mgA2IEC9iJ4lolb.jyo45DVxN2UKSoiULy93RMJNqRC2T8m', 'Anna', 'Kovács', '+36201234567', '1985-08-20', '', '0000-00-00 00:00:00', 1, '', '0000-00-00 00:00:00', 0, 0),
-(7, 'test1@gmail.com', '$2y$10$LkFty/N3eEPq3amLbgKKd.eUiHky/2wiIwG452ARLluYAIUvnNVea', 'Anna', 'Kovács', '+36201234567', '1985-08-20', '4e0ea3a7d695bf3ce3c448dd23069671a88034d5b13e778dd5dd818ca4f092e5', '2025-08-31 15:09:14', 1, 'b44b31d1093af3a9e95dc58e6bb5cc885ec5d9fac1f703b7b299f0f6531312a0', '2025-08-31 01:11:24', 0, 0),
+(7, 'test1@gmail.com', '$2y$10$LkFty/N3eEPq3amLbgKKd.eUiHky/2wiIwG452ARLluYAIUvnNVea', 'Anna', 'Kovács', '+36201234567', '1985-08-20', '4e0ea3a7d695bf3ce3c448dd23069671a88034d5b13e778dd5dd818ca4f092e5', '2025-08-31 15:09:14', 1, 'b44b31d1093af3a9e95dc58e6bb5cc885ec5d9fac1f703b7b299f0f6531312a0', '2025-08-31 01:11:24', 0, 1),
 (8, 'test2@gmail.com', '$2y$10$u5RI43wRrPMBqoWbZ5.hFel8opCY0PZx.6K9g/Yv954y5EbL/Jdr.', 'Test2', 'Test2', '0650975642', '2003-07-08', '', '0000-00-00 00:00:00', 1, '', '0000-00-00 00:00:00', 0, 0),
 (9, 'test3@gmail.com', '$2y$10$060JDWhTGfvs.HczZTBMI.lFS.QPILhs/MVFpDuqpyAR2KhNG1Ydq', 'Test3', 'Test3', '1234567891', '2002-11-12', '', '0000-00-00 00:00:00', 1, '', '0000-00-00 00:00:00', 0, 0);
 
@@ -127,7 +158,9 @@ CREATE TABLE `owner_pet` (
 INSERT INTO `owner_pet` (`owner_id`, `pet_id`) VALUES
 (5, 7),
 (5, 8),
-(6, 9);
+(7, 10),
+(7, 18),
+(8, 19);
 
 -- --------------------------------------------------------
 
@@ -152,7 +185,10 @@ CREATE TABLE `pet` (
 INSERT INTO `pet` (`pet_id`, `name`, `species`, `breed`, `gender`, `birth_date`, `vet_id`) VALUES
 (7, 'Bajnok', 'dog', 'puli', 'male', '2021-05-01', 1),
 (8, 'Bodri', 'kutya', 'golden retriever', '', '2021-05-01', 1),
-(9, 'Bajnok1', 'dog', 'puli1', 'male', '2021-05-01', 1);
+(9, 'Bajnok1', 'dog', 'puli1', 'male', '2021-05-01', 1),
+(10, 'test11', 'kutyaaaa', 'valamiaaa', 'male', '2023-07-08', 2),
+(18, 'asd', 'asd', 'asd', 'male', '1999-07-08', 1),
+(19, 'test2', 'asd', 'asd', '', '1999-07-08', 1);
 
 -- --------------------------------------------------------
 
@@ -172,7 +208,8 @@ CREATE TABLE `treatment` (
 --
 
 INSERT INTO `treatment` (`treatment_id`, `name`, `duration_min`, `cost`) VALUES
-(1, 'test1', 15, 2500);
+(1, 'test1', 15, 2500),
+(2, 'hosszu', 60, 2500);
 
 -- --------------------------------------------------------
 
@@ -196,7 +233,8 @@ CREATE TABLE `vet` (
 --
 
 INSERT INTO `vet` (`vet_id`, `email`, `password`, `first_name`, `last_name`, `birth_date`, `phone_number`, `specialization`) VALUES
-(1, 'testvet@gmail.com', 'blablabla', 'asdas', 'asdasd', '2019-07-18', '123456789', 'blabla');
+(1, 'testvet@gmail.com', '$2y$10$ApBm65VZoixP7bdmby5Z9eq0yo.C661jBu2xrIIsg8hKv2EThPyRG', 'asdasasd', 'Nagy1', '2019-07-18', '123456789', 'blabla'),
+(2, 'testvet2@gmail.com', '$2y$10$060JDWhTGfvs.HczZTBMI.lFS.QPILhs/MVFpDuqpyAR2KhNG1Ydq', 'test2', 'vet2', '2025-08-05', '+3620123456', 'ajajaj');
 
 -- --------------------------------------------------------
 
@@ -218,7 +256,12 @@ CREATE TABLE `vet_schedule` (
 --
 
 INSERT INTO `vet_schedule` (`schedule_id`, `vet_id`, `weekday`, `start_time`, `end_time`, `slot_minutes`) VALUES
-(1, 1, 1, '09:00:00', '18:00:00', 30);
+(1, 1, 1, '09:00:00', '18:00:00', 30),
+(11, 2, 1, '09:00:00', '18:00:00', 15),
+(12, 2, 2, '06:00:00', '18:00:00', 15),
+(13, 2, 3, '09:00:00', '22:00:00', 30),
+(14, 2, 4, '09:00:00', '19:00:00', 30),
+(15, 2, 5, '10:00:00', '20:00:00', 30);
 
 --
 -- Indexek a kiírt táblákhoz
@@ -295,19 +338,19 @@ ALTER TABLE `vet_schedule`
 -- AUTO_INCREMENT a táblához `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT a táblához `appointment`
 --
 ALTER TABLE `appointment`
-  MODIFY `appointment_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `appointment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT a táblához `log`
 --
 ALTER TABLE `log`
-  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT a táblához `owner`
@@ -319,25 +362,25 @@ ALTER TABLE `owner`
 -- AUTO_INCREMENT a táblához `pet`
 --
 ALTER TABLE `pet`
-  MODIFY `pet_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `pet_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT a táblához `treatment`
 --
 ALTER TABLE `treatment`
-  MODIFY `treatment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `treatment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT a táblához `vet`
 --
 ALTER TABLE `vet`
-  MODIFY `vet_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `vet_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT a táblához `vet_schedule`
 --
 ALTER TABLE `vet_schedule`
-  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Megkötések a kiírt táblákhoz
